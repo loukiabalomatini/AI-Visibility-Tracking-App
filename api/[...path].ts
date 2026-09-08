a48ae176b424f4b4f550b07e950f69ac99b000f0
Loukia Balomatini <79973507+loukiabalomatini@users.noreply.github.com>
2026-09-08 11:40:46 +0300

    fix: use explicit server entrypoint

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { createApp } from '../server';
+import { createApp } from '../server.ts';
 
 export default async function handler(
   req: VercelRequest,
