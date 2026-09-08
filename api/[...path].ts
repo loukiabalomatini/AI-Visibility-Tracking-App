b242e4d00d426fbc706d0f9a68636fbdec104288
Loukia Balomatini <79973507+loukiabalomatini@users.noreply.github.com>
2026-09-08 11:36:33 +0300

    Update [...path].ts

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { createApp } from '../server.ts';
+import { createApp } from '../server';
 
 export default async function handler(
   req: VercelRequest,
